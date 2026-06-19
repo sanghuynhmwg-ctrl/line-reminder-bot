1aff83340455ccc57cb3271fc604b66afa70caf7
sanghuynhmwg-ctrl <sanghuynh.mwg@gmail.com>
2026-06-19 12:13:43 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-TOKEN = os.environ["Au/1SVffTM/zzIqPGo0JpTLiNQ4ceQtC+zMcQx/dW1VyzFeUsHmvELUfMPmC1lvVPokZ8BNkek/bF0m5qhjaJBPdaJD8YWW9eJVB8aPnS7+4aoscrCV07EEFVFZYuM4J5pa9CdXuWiWDvo6GBqBApgdB04t89/1O/w1cDnyilFU="]
-GROUP_ID = os.environ["C0dc7443ed017d2175c3640e35da88eb9"]
+TOKEN = os.environ["LINE_TOKEN"]
+GROUP_ID = os.environ["GROUP_ID"]
 
 headers = {
     "Authorization": f"Bearer {TOKEN}",
